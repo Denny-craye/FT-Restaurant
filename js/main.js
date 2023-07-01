19912fbb1e918a039b23919da046abcc436d6a1a
Denny_cry <dima.dimasik.ru@mail.ru>
2023-07-01 03:08:14 +0300

    test media queries

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ const clearInput = () => {
   
   const clearBtn = document.getElementById("clear-btn");
   clearBtn.addEventListener("click", clearInput);
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("drop-menu").addEventListener("click", function()
+    {
+        document.querySelector(".header").classList.toggle("open")
+    })
+})
